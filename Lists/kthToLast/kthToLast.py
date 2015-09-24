03c3ba8cc19b7cc7921b988452dbf41d8202e9ab
Pratham Tandon <prathamt@outlook.com>
2015-09-24 11:03:32 -0700

    Fixed formatting

--- Lists/kthToLast/kthToLast.py
+++ Lists/kthToLast/kthToLast.py
@@ -1,15 +1,15 @@
 # Finds the kth to last element in a linked list
 
 def kthToLast(lst, k):
-	count = 0
-	first = lst.head
-	while count < k:
-		if first is None:
-			return None
-		first = first.get_next()
-		count += 1
-	second = lst.head
-	while first is not None:
-		first = first.get_next()
-		second = second.get_next()
-	return second
+    count = 0
+    first = lst.head
+    while count < k:
+        if first is None:
+            return None
+        first = first.get_next()
+        count += 1
+    second = lst.head
+    while first is not None:
+        first = first.get_next()
+        second = second.get_next()
+    return second
