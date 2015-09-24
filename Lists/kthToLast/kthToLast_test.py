32ccb81ac312e932f5c67a836233a14e7b5943d4
Pratham Tandon <prathamt@outlook.com>
2015-09-24 11:02:55 -0700

    Add kthToLast and test cases

---
+++ Lists/kthToLast/kthToLast_test.py
@@ -0,0 +1,25 @@
+from LinkedList import LinkedList
+from kthToLast import kthToLast
+import pytest
+
+def test_kthToLast():
+	lst = LinkedList()
+	lst.insert("Batman")
+	lst.insert("Superman")
+	lst.insert("Flash")
+	lst.insert("Green Lantern")
+	lst.insert("Wonder Woman")
+	lst.insert("Hawkgirl")
+	# Hawkgirl -> Wonder Woman -> ... -> Batman -> None
+	_2nd_last = kthToLast(lst, 2)
+	_6th_last = kthToLast(lst, 6)
+	_1st_last = kthToLast(lst, 1)
+	_overflow = kthToLast(lst, 10)
+	_underflow = kthToLast(lst, -1)
+	assert(_2nd_last.get_key() == "Superman")
+	assert(_6th_last.get_key() == "Hawkgirl")
+	assert(_1st_last.get_key() == "Batman")
+	assert(_overflow is None)
+	assert(_underflow is None)
+	
+	
