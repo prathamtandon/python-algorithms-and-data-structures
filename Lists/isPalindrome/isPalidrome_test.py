6b2a9f87ab542b2018d7cd5bb9a1fd1ef7ff8f43
Pratham Tandon <prathamt@outlook.com>
2015-09-24 13:05:41 -0700

    Add another approach for isPalindrome and test cases

--- Lists/isPalindrome/isPalidrome_test.py
+++ Lists/isPalindrome/isPalidrome_test.py
@@ -1,5 +1,5 @@
 from LinkedList import LinkedList
-from isPalindrome import isPalindrome
+from isPalindrome import isPalindrome, isPalidrome_iterative
 
 def test_isPalindrome():
     lst1 = LinkedList()
@@ -8,6 +8,7 @@ def test_isPalindrome():
     lst1.insert(2)
     # 2 -> 5 -> 3 -> None
     assert(isPalindrome(lst1) is False)
+    assert(isPalidrome_iterative(lst1) is False)
 
     lst2 = LinkedList()
     lst2.insert(3)
@@ -18,3 +19,4 @@ def test_isPalindrome():
     lst2.insert(3)
     # 3 -> 3 -> 5 -> 5 -> 3 -> 3 -> None
     assert(isPalindrome(lst2) is True)
+    assert(isPalidrome_iterative(lst2) is True)
--- Lists/isPalindrome/isPalindrome.py
+++ Lists/isPalindrome/isPalindrome.py
@@ -23,3 +23,35 @@ def is_equal_list(list1, list2):
 def isPalindrome(lst):
     reverse_lst = reverse(lst)
     return is_equal_list(lst, reverse_lst)
+
+
+# Approach 2: Use fast and slow pointers where fast moves at
+# twice the speed of slow. So, when fast reaches end of list,
+# slow should be at the middle.
+# Now if we know the reverse of first half of list, we could
+# compare it with remaining half and if they are equal, then
+# given list is palindrome. This approach is slightly more space
+# efficient as it would still use a Stack to get the reverse of first
+# half.
+
+def isPalidrome_iterative(lst):
+    slow = lst.head
+    fast = lst.head
+
+    stack = []
+
+    while fast is not None and fast.get_next() is not None:
+        stack.append(slow.get_key())
+        slow = slow.get_next()
+        fast = fast.get_next().get_next()
+
+    # When there are odd number of nodes, skip the middle.
+    if fast is not None:
+        slow = slow.get_next()
+
+    while slow is not None:
+        if slow.get_key() != stack.pop():
+            return False
+        slow = slow.get_next()
+
+    return True
