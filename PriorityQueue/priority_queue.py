b64efffb9e7ab4f819d8af4b608e6fd89e6f5165
Pratham Tandon <prathamt@outlook.com>
2015-09-27 21:23:34 -0700

    Fixed formatting

--- PriorityQueue/priority_queue.py
+++ PriorityQueue/priority_queue.py
@@ -1,58 +1,58 @@
 from math import floor;
 
 class PriorityQueue:
-	
-	def __init__(self):
-		self.items = []
-	
-	def parent(self, i):
-		return int(floor(i/2))
-	
-	def left(self, i):
-		return 2*i+1
-	
-	def right(self, i):
-		return 2*i+2
-	
-	def max_heapify(self, i):
-		l = self.left(i)
-		r = self.right(i)
-		largest = i
-		
-		if l < len(self.items) and self.items[l] > self.items[largest]:
-			largest = l
-		if r < len(self.items) and self.items[r] > self.items[largest]:
-			largest = r
-		
-		if largest != i:
-			temp = self.items[i]
-			self.items[i] = self.items[largest]
-			self.items[largest] = self.items[i]
-			self.max_heapify(largest)
-	
-	def get_max(self):
-		return self.items[0]
-	
-	def extract_max(self):
-		max_priority = self.items[0]
-		self.items[0] = self.items[len(self.items)-1]
-		self.items.pop()
-		self.max_heapify(0)
-		return max_priority
-	
-	def update_key(self, key, index):
-		self.items[index] = key
-		i = index
-		while i >= 0 and self.items[self.parent(i)] < self.items[i]:
-			temp = self.items[self.parent(i)]
-			self.items[self.parent(i)] = self.items[i]
-			self.items[i] = temp
-			i = self.parent(i)
-	
-	def insert_key(self, key):
-		self.items.append(-1)
-		self.update_key(key, len(self.items)-1)
-	
-	def contains(self, key):
-		return key in self.items
-		
+
+    def __init__(self):
+        self.items = []
+
+    def parent(self, i):
+        return int(floor(i/2))
+
+    def left(self, i):
+        return 2*i+1
+
+    def right(self, i):
+        return 2*i+2
+
+    def max_heapify(self, i):
+        l = self.left(i)
+        r = self.right(i)
+        largest = i
+
+        if l < len(self.items) and self.items[l] > self.items[largest]:
+            largest = l
+        if r < len(self.items) and self.items[r] > self.items[largest]:
+            largest = r
+
+        if largest != i:
+            temp = self.items[i]
+            self.items[i] = self.items[largest]
+            self.items[largest] = self.items[i]
+            self.max_heapify(largest)
+
+    def get_max(self):
+        return self.items[0]
+
+    def extract_max(self):
+        max_priority = self.items[0]
+        self.items[0] = self.items[len(self.items)-1]
+        self.items.pop()
+        self.max_heapify(0)
+        return max_priority
+
+    def update_key(self, key, index):
+        self.items[index] = key
+        i = index
+        while i >= 0 and self.items[self.parent(i)] < self.items[i]:
+            temp = self.items[self.parent(i)]
+            self.items[self.parent(i)] = self.items[i]
+            self.items[i] = temp
+            i = self.parent(i)
+
+    def insert_key(self, key):
+        self.items.append(-1)
+        self.update_key(key, len(self.items)-1)
+
+    def contains(self, key):
+        return key in self.items
+
--- PriorityQueue/priority_queue_test.py
+++ PriorityQueue/priority_queue_test.py
@@ -2,33 +2,33 @@ from priority_queue import PriorityQueue
 import pytest
 
 def test_insert_key():
-	pq = PriorityQueue()
-	pq.insert_key(24)
-	pq.insert_key(12)
-	pq.insert_key(1)
-	pq.insert_key(35)
-	
-	assert(pq.contains(24) == True)
-	assert(pq.contains(12) == True)
-	assert(pq.contains(1) == True)
-	assert(pq.contains(35) == True)
-	assert(pq.contains(10) == False)
+    pq = PriorityQueue()
+    pq.insert_key(24)
+    pq.insert_key(12)
+    pq.insert_key(1)
+    pq.insert_key(35)
+
+    assert(pq.contains(24) == True)
+    assert(pq.contains(12) == True)
+    assert(pq.contains(1) == True)
+    assert(pq.contains(35) == True)
+    assert(pq.contains(10) == False)
 
 def test_get_max():
-	pq = PriorityQueue()
-	pq.insert_key(24)
-	pq.insert_key(12)
-	pq.insert_key(1)
-	pq.insert_key(35)
-	
-	assert(pq.get_max() == 35)
+    pq = PriorityQueue()
+    pq.insert_key(24)
+    pq.insert_key(12)
+    pq.insert_key(1)
+    pq.insert_key(35)
+
+    assert(pq.get_max() == 35)
 
 def test_extract_max():
-	pq = PriorityQueue()
-	pq.insert_key(24)
-	pq.insert_key(12)
-	pq.insert_key(1)
-	pq.insert_key(35)
-	
-	assert(pq.extract_max() == 35)
-	assert(pq.extract_max() == 24)
+    pq = PriorityQueue()
+    pq.insert_key(24)
+    pq.insert_key(12)
+    pq.insert_key(1)
+    pq.insert_key(35)
+
+    assert(pq.extract_max() == 35)
+    assert(pq.extract_max() == 24)
