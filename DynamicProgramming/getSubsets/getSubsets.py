2df6c079c501b551be2580078f978a0374ac83c1
Pratham Tandon <prathamt@outlook.com>
2015-10-08 10:42:51 -0700

    Add method#2 for getSubsets

--- DynamicProgramming/getSubsets/getSubsets.py
+++ DynamicProgramming/getSubsets/getSubsets.py
@@ -13,7 +13,31 @@ def getSubsets_helper(input_set, n):
             cloned = list(subset)
             cloned.append(n)
             moreSubsets.append(cloned)
-        print str(allSubsets)
-        print str(moreSubsets)
         return allSubsets + moreSubsets
 
+# Method 2
+# If we look at any given subset of some set, we could think
+# of each element in the subset as a "yes" and each element from
+# the original set not present in the subset as a "no". So each
+# subset is just a sequence of "yes" and "no". We could
+# represent each subset, therefore, as a sequence of binary numbers
+# from 0 to 2^n (exclusive) where n is the size of set.
+
+def getSubsets2(input_set):
+    allSubsets = []
+    max_val = 1 << len(input_set)
+    for k in range(0, max_val):
+        subset = convertFromIntToSet(k, input_set)
+        allSubsets.append(subset)
+    return allSubsets
+
+def convertFromIntToSet(x, input_set):
+    subset = []
+    index = 0
+    k = x
+    while k > 0:
+        if k & 1 == 1:
+            subset.append(input_set[index])
+        index += 1
+        k >>= 1
+    return subset
--- DynamicProgramming/getSubsets/getSubsets_test.py
+++ DynamicProgramming/getSubsets/getSubsets_test.py
@@ -1,4 +1,5 @@
 from getSubsets import getSubsets
+from getSubsets import getSubsets2
 import pytest
 
 def test_getSubsets():
@@ -13,3 +14,18 @@ def test_getSubsets():
     assert([1] in subsets_2)
     assert([2] in subsets_2)
     assert([1,2] in subsets_2)
+
+
+def test_getSubsets2():
+    assert(getSubsets2([]) == [[]])
+    subsets_1 = getSubsets2([1])
+    assert(len(subsets_1) == 2)
+    assert([] in subsets_1)
+    assert([1] in subsets_1)
+    subsets_2 = getSubsets2([1,2])
+    assert(len(subsets_2) == 4)
+    assert([] in subsets_2)
+    assert([1] in subsets_2)
+    assert([2] in subsets_2)
+    assert([1,2] in subsets_2)
+
